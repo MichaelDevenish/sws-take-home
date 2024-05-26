dad0ce841b0654bf30d59891bfd5d97ecf778adc
Michael Devenish <iammichaeldeve@gmail.com>
2024-05-26 10:31:54 +1000

    Add more test cases.

--- src/resolvers/company/company.test.ts
+++ src/resolvers/company/company.test.ts
@@ -1,6 +1,47 @@
 import {app} from "../../app";
 import request from "supertest";
 
+
+const allCompanyDetails = `
+  id
+  name
+  tickerSymbol
+  exchangeSymbol
+  uniqueSymbol
+  dateGenerated
+  securityName
+  exchangeCountryIso
+  listingCurrencyIso
+  canonicalUrl
+  uniqueSymbolSlug
+  scoreId
+  companyPriceClose {
+    date
+    companyId
+    price
+    dateCreated
+  }
+  companyPriceCloses {
+    date
+    companyId
+    price
+    dateCreated
+  }
+  companyScore {
+    id
+    companyId
+    dateGenerated
+    future
+    health
+    management
+    past
+    value
+    misc
+    total
+    sentence
+  }
+`
+
 describe("Test the company api", () => {
     test("It should list companies", async () => {
         const query =  `
@@ -35,43 +76,7 @@ describe("Test the company api", () => {
         const query =  `
           query test {
             getCompany(companyId: "4BE2C01F-F390-479C-A166-8E0DD73CF7B9") {
-              id
-              name
-              tickerSymbol
-              exchangeSymbol
-              uniqueSymbol
-              dateGenerated
-              securityName
-              exchangeCountryIso
-              listingCurrencyIso
-              canonicalUrl
-              uniqueSymbolSlug
-              scoreId
-              companyPriceClose {
-                date
-                companyId
-                price
-                dateCreated
-              }
-              companyPriceCloses {
-                date
-                companyId
-                price
-                dateCreated
-              }
-              companyScore {
-                id
-                companyId
-                dateGenerated
-                future
-                health
-                management
-                past
-                value
-                misc
-                total
-                sentence
-              }
+              ${allCompanyDetails}
             }
           }
         `
@@ -101,5 +106,23 @@ describe("Test the company api", () => {
 
         expect(response.body).toMatchSnapshot()
         expect(response.status).toEqual(400)
+    });
+
+    test("It should be able to fetch a company with NO relations", async () => {
+        const query =  `
+          query test {
+            getCompany(companyId: "65DCE5EA-70D6-417F-9CAC-1EAA92FB7F1C") {
+              ${allCompanyDetails}
+            }
+          }
+        `
+        const response = await request(app)
+            .post("/graphql")
+            .send({query })
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+
+        expect(response.body).toMatchSnapshot()
+        expect(response.status).toEqual(200)
     });
 });
